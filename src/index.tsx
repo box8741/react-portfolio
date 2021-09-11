c217279ac862fe01f460684784098441339d55ba
box8741 <box8741@gmail.com>
2021-09-11 15:39:04 +0900

    fix : IE redirect 문제 수정

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,18 @@ import 'react-app-polyfill/stable'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {CssBaseline} from '@material-ui/core'
+import {isIE} from 'react-device-detect'
 
 import mkUtils from './common/utils'
 import GlobalStyle from './common/GlobalStyle'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
-if (mkUtils.Browser === 'IE') {
+if (isIE) {
   location.href = 'microsoft-edge:' + window.location
   setTimeout(function () {
     location.href = 'https://go.microsoft.com/fwlink/?linkid=2135547'
-  })
+  }, 500)
 }
 
 ReactDOM.render(
